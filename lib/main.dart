f1d11842b45a6d6c09e2088e44809cf2fc92262f
Stefano <stefano.ventrudo10@gmail.com>
2024-08-15 12:04:57 +0200

    Show map in a new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:exlorer_flutter/full_map.dart';
+import 'package:exlorer_flutter/page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -55,17 +57,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> _pushPage(BuildContext context, ExamplePage page) async {
+    if (context.mounted) {
+      Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (_) => Scaffold(
+          appBar: AppBar(title: Text(page.title)),
+          body: page,
+        ),
+      ));
+    }
   }
 
   @override
@@ -105,20 +106,19 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
+              'Welcome Explorer!',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () => {
+          _pushPage(context, const FullMapPage())
+        },
+        tooltip: 'Show map',
+        child: const Icon(Icons.map),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
